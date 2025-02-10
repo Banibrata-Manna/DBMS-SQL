15471d8a1444ffe9ffef79824175f8dd8ed324a2
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-10 15:04:48 +0530

    Question 5
    Marked for recheck of joins

--- SQL-Assignment1.sql
+++ SQL-Assignment1.sql
@@ -53,5 +53,64 @@ join good_identification gi on p.PRODUCT_ID = gi.PRODUCT_ID
 and gi.GOOD_IDENTIFICATION_TYPE_ID = 'ERP_ID' and gi.ID_VALUE  is null;
 
 
+-- 5 Completed Orders in August 2023
+-- Business Problem:
+-- After running similar reports for a previous month,
+-- you now need all completed orders in August 2023 for analysis.
+-- 
+-- Fields to Retrieve:
+-- 
+-- PRODUCT_ID
+-- PRODUCT_TYPE_ID
+-- PRODUCT_STORE_ID
+-- TOTAL_QUANTITY
+-- INTERNAL_NAME
+-- FACILITY_ID
+-- EXTERNAL_ID
+-- FACILITY_TYPE_ID
+-- ORDER_HISTORY_ID
+-- ORDER_ID
+-- ORDER_ITEM_SEQ_ID
+-- SHIP_GROUP_SEQ_ID
+
+
+
+select oi.PRODUCT_ID , p.PRODUCT_TYPE_ID , psc.PRODUCT_STORE_ID, oi.QUANTITY, oi.EXTERNAL_ID ,
+oi.ORDER_ID , oi.ORDER_ITEM_SEQ_ID , oisg.FACILITY_ID, oi.EXTERNAL_ID ,
+oh.ORDER_HISTORY_ID , oisg.SHIP_GROUP_SEQ_ID  from
+order_item oi 
+join
+order_status os on os.ORDER_ID = oi.ORDER_ID
+and os.STATUS_ID = 'ORDER_COMPLETED' and os.STATUS_DATETIME >= '2023-08-01'
+and os.STATUS_DATETIME <= '2023-08-31'
+join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID -- joining items and ship group by orderId
+join product_store_facility psf on oisg.FACILITY_ID = psf.FACILITY_ID and psf.THRU_DATE is null -- joining the stores which use the facility mentioned in ship group 
+join product_store_catalog psc on oi.PROD_CATALOG_ID = psc.PROD_CATALOG_ID and psc.THRU_DATE is null
+and psc.PRODUCT_STORE_ID = psf.PRODUCT_STORE_ID -- joining the stores that have the similar catalog of product item
+join product p on p.PRODUCT_ID = oi.PRODUCT_ID
+join order_history oh on oi.ORDER_ID = oh.ORDER_ID;
+
+select ps.PRODUCT_STORE_ID as ps, psc.PROD_CATALOG_ID as pci, psf.FACILITY_ID as fi from product_store ps 
+join product_store_catalog psc using(PRODUCT_STORE_ID)
+join product_store_facility psf using(PRODUCT_STORE_ID)
+where psf.THRU_DATE is null and psc.THRU_DATE is null;
+
+select oi.PRODUCT_ID , p.PRODUCT_TYPE_ID , product_stores.ps, oi.QUANTITY, oi.EXTERNAL_ID ,
+oi.ORDER_ID , oi.ORDER_ITEM_SEQ_ID , oisg.FACILITY_ID, oi.EXTERNAL_ID ,
+oh.ORDER_HISTORY_ID , oisg.SHIP_GROUP_SEQ_ID  from
+order_item oi 
+join
+order_status os on os.ORDER_ID = oi.ORDER_ID
+and os.STATUS_ID = 'ORDER_COMPLETED' and os.STATUS_DATETIME >= '2023-08-01'
+and os.STATUS_DATETIME <= '2023-08-31'
+join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID -- joining items and ship group by orderId
+join (select ps.PRODUCT_STORE_ID as ps, psc.PROD_CATALOG_ID as pci, psf.FACILITY_ID as fi from product_store ps 
+join product_store_catalog psc using(PRODUCT_STORE_ID)
+join product_store_facility psf using(PRODUCT_STORE_ID)
+where psf.THRU_DATE is null and psc.THRU_DATE is null) as product_stores on product_stores.pci = oi.PROD_CATALOG_ID and product_stores.fi = oisg.FACILITY_ID 
+join product p on p.PRODUCT_ID = oi.PRODUCT_ID
+join order_history oh on oi.ORDER_ID = oh.ORDER_ID;
+
+
 
 
