c2145a9d0c51fd7d101a8c15d9ff83262d82ad2e
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-10 15:43:54 +0530

    Modified join between product store details and order details
    added one more condition in ON clause of shipGroupSeqId

--- SQL-Assignment1.sql
+++ SQL-Assignment1.sql
@@ -81,12 +81,13 @@ join
 order_status os on os.ORDER_ID = oi.ORDER_ID
 and os.STATUS_ID = 'ORDER_COMPLETED' and os.STATUS_DATETIME >= '2023-08-01'
 and os.STATUS_DATETIME <= '2023-08-31'
-join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID -- joining items and ship group by orderId
+join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID and oi.SHIP_GROUP_SEQ_ID = oisg.SHIP_GROUP_SEQ_ID-- joining items and ship group by orderId
 join product_store_facility psf on oisg.FACILITY_ID = psf.FACILITY_ID and psf.THRU_DATE is null -- joining the stores which use the facility mentioned in ship group 
 join product_store_catalog psc on oi.PROD_CATALOG_ID = psc.PROD_CATALOG_ID and psc.THRU_DATE is null
 and psc.PRODUCT_STORE_ID = psf.PRODUCT_STORE_ID -- joining the stores that have the similar catalog of product item
 join product p on p.PRODUCT_ID = oi.PRODUCT_ID
 join order_history oh on oi.ORDER_ID = oh.ORDER_ID;
+
 
 
 select oi.PRODUCT_ID , p.PRODUCT_TYPE_ID , product_stores.ps, oi.QUANTITY, oi.EXTERNAL_ID ,
@@ -96,8 +97,8 @@ order_item oi
 join
 order_status os on os.ORDER_ID = oi.ORDER_ID
 and os.STATUS_ID = 'ORDER_COMPLETED' and os.STATUS_DATETIME >= '2023-08-01'
-and os.STATUS_DATETIME <= '2023-08-31'
-join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID -- joining items and ship group by orderId
+and os.STATUS_DATETIME <= '2023-08-31' 
+join order_item_ship_group oisg on oi.ORDER_ID = oisg.ORDER_ID and oi.SHIP_GROUP_SEQ_ID = oisg.SHIP_GROUP_SEQ_ID -- joining items and ship group by orderId
 join (select ps.PRODUCT_STORE_ID as ps, psc.PROD_CATALOG_ID as pci, psf.FACILITY_ID as fi from product_store ps 
 join product_store_catalog psc using(PRODUCT_STORE_ID)
 join product_store_facility psf using(PRODUCT_STORE_ID)
