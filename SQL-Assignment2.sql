613cc711cea5351e6e11a1c0948f757a1efe0708
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-11 18:13:03 +0530

    SQL2 7.3 Solution

--- SQL-Assignment2.sql
+++ SQL-Assignment2.sql
@@ -101,5 +101,31 @@ select *
 from product_sales 
 where TOTAL_QUANTITY_SOLD > AVG_QUANTITY_SOLD;
 
+-- 7.3 Store-Specific (Facility-Wise) Revenue
+-- Business Problem:
+-- Different physical or online stores (facilities) may have varying levels of performance. The business wants to compare revenue across facilities for sales planning and budgeting.
+-- 
+-- Fields to Retrieve:
+-- 
+-- FACILITY_ID
+-- FACILITY_NAME
+-- TOTAL_ORDERS
+-- TOTAL_REVENUE
+-- DATE_RANGE
+
+select 
+    f.facility_id as facility_id,
+    f.facility_name as facility_name,
+    count(distinct oh.order_id) as total_orders,
+    sum(oh.grand_total) as total_revenue,
+    concat(min(oh.entry_date), ' to ', max(oh.entry_date)) as date_range
+from order_header oh
+join order_item_ship_group oisg 
+    on oh.order_id = oisg.order_id and oh.ENTRY_DATE >= '2020-01-01 00:00:00' and
+oh.ENTRY_DATE <= '2020-01-31 00:00:00' and oh.status_id = 'ORDER_COMPLETED'
+join facility f 
+    on oisg.facility_id = f.facility_id 
+group by f.facility_id, f.facility_name;
+
 
 
