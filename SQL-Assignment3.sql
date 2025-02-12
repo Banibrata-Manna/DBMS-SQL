011fcb533eb58650a1c8d8429fb42f7f81cf06b0
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-12 16:53:00 +0530

    SQL3 Question 2

--- SQL-Assignment3.sql
+++ SQL-Assignment3.sql
@@ -40,6 +40,45 @@ join
 product_type pt on p.PRODUCT_TYPE_ID = pt.PRODUCT_TYPE_ID;
 
 
+-- 2 Completed Return Items
+-- Business Problem:
+-- Customer service and finance often need insights into
+-- returned items to manage refunds, replacements, and inventory restocking.
+-- 
+-- Fields to Retrieve:
+-- 
+-- RETURN_ID
+-- ORDER_ID
+-- PRODUCT_STORE_ID
+-- STATUS_DATETIME
+-- ORDER_NAME
+-- FROM_PARTY_ID
+-- RETURN_DATE
+-- ENTRY_DATE
+-- RETURN_CHANNEL_ENUM_ID
+
+select
+	rh.RETURN_ID ,
+	ri.ORDER_ID ,
+	oh.PRODUCT_STORE_ID ,
+	rs.STATUS_ID ,
+	rs.STATUS_DATETIME ,
+	oh.ORDER_NAME ,
+	rh.FROM_PARTY_ID ,
+	rh.RETURN_DATE ,
+	rh.ENTRY_DATE ,
+	rh.RETURN_CHANNEL_ENUM_ID 
+from 
+return_header rh 
+join 
+return_item ri on rh.RETURN_ID = ri.RETURN_ID 
+join 
+return_status rs on rh.RETURN_ID = rs.RETURN_ID 
+and ri.RETURN_ITEM_SEQ_ID = rs.RETURN_ITEM_SEQ_ID 
+join 
+order_header oh on ri.ORDER_ID = oh.ORDER_ID;
+
+
 
 
 
